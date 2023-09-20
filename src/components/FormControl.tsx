e477fefd4038c389f8f96f1ae4667da324bc1426
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2023-09-20 14:33:03 +0300

    fix scrap

--- src/components/FormControl.tsx
+++ src/components/FormControl.tsx
@@ -11,11 +11,11 @@ const FormControl = ({ id, title, placeholder, error }: FormControlProps) => {
     return (
         <label htmlFor={id} className='flex flex-col'>
             <div className="mb-1 flex justify-between">
-                <span className="font-bold text-blue-900">{title}</span>
+                <span className="text-blue-900">{title}</span>
                 {error && <span className="font-bold text-red-500">{error}</span>}
             </div>
-            <input className="w-full border rounded-md border-gray-300 py-2 px-4 text-xl font-bold text-blue-900 
-                                    focus:border-indigo-400 outline-none"
+            <input className="w-full border rounded-md border-gray-300 py-3 px-4 text-md font-bold text-blue-900 
+                            focus:border-indigo-400 outline-none"
                 placeholder={placeholder} id={id} />
         </label>
 
--- src/components/PlanCard.tsx
+++ src/components/PlanCard.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
+
 const PlanCard = () => {
     return (
         <div className="flex-auto h-[160px] border rounded-lg border-gray-300 px-3 pt-4 pb-2 flex flex-col cursor-pointer hover:border-indigo-400">
-            <img src="../../public/images/icon-arcade.svg" alt="Arcade"/>
+            <img src={window.location.origin + "/images/icon-arcade.svg"} alt="Arcade" className="w-[40px] h-[40px]"/>
             <div className="mt-auto">
                 <h2 className="font-bold text-blue-900">Arcade</h2>
                 <p className="text-gray-400">$9/mo</p>
